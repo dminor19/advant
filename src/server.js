37604914e93a4452fde501d6cf4e8e95c56e1375
Devon Mino <devon.minor.00@gmail.com>
2021-02-04 13:58:23 -0500

    added default route app.get('/',...);

--- src/server.js
+++ src/server.js
@@ -29,6 +29,10 @@ const startServer = async () => {
 
     const port = process.env.PORT || 4000;
 
+    app.get('/', (req, res) => {
+        res.send('hello');
+    });
+
     app.listen({ port },
         () => console.log(`Server running at http://localhost:${port}${server.graphqlPath}`));
 }
